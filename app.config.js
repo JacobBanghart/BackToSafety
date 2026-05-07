f724dbce12e1fdd4eb8ef2357c6b8bed782e7a58
JacobBanghart <jacobmbanghart@gmail.com>
2026-05-06 22:14:53 -0700

    chore: remove PostHog wizard leftovers

--- app.config.js
+++
@@ -1,9 +0,0 @@
-const appJson = require('./app.json');
-
-module.exports = {
-  ...appJson.expo,
-  extra: {
-    posthogProjectToken: process.env.POSTHOG_PROJECT_TOKEN,
-    posthogHost: process.env.POSTHOG_HOST,
-  },
-};
